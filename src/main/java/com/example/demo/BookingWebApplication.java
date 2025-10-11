709daa44bfd442842d209fd5c767b766e916ecc6
sushantbiramane <biramanesushant94@gmail.com>
2025-10-11 12:11:18 +0530

    Update BookingWebApplication.java

--- src/main/java/com/example/demo/BookingWebApplication.java
+++ src/main/java/com/example/demo/BookingWebApplication.java
@@ -1,3 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class BookingWebApplication extends SpringBootServletInitializer {
 
